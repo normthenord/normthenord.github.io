33222bb0266bbf87c0b9896bd080124a09b13754
Stephen Burwell <xstompx@gmail.com>
2020-11-01 16:20:45 -0500

    Let's see if this works

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ async function getRandomDogs() {
   }
   else{
     API_URL = 'https://dog.ceo/api/breeds/image/random/3';
-    console.log("boop")
   }
   const response = await fetch(API_URL);
   const json = await response.json();
